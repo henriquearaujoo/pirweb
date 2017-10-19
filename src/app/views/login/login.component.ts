b6760970852d896f511c0dc9c349274f69c4005f
Fabricia Galibi <fabricia.galibi@institutoitn.com.br>
2017-10-19 10:38:33 -0400

    incluido api test para get

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../services/login/authentication.service';
 import { User } from '../../models/user';
 
+import { RestService } from '../../services/rest/rest.service'
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,16 +12,28 @@ import { User } from '../../models/user';
 })
 export class LoginComponent implements OnInit {
 
-  private user: User = new User(1,'','')
+  private user: User = new User(1, '', '')
 
-  constructor( private authenticationService: AuthenticationService ) { }
+  constructor(private authenticationService: AuthenticationService,
+      private restService: RestService) { }
 
   ngOnInit() {
   }
 
-  login(){
+  login() {
     console.log(this.user)
     this.authenticationService.login(this.user)
+  }
+
+  apiTest() {
+    this.restService.getApiTest().toPromise()
+    .then(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.error(error);
+      });
   }
 
 }
