114ee0a83eb036dc72ff8a24db963e7025650b91
luiz ricardo <luiz.ricardo@institutoitn.com.br>
2017-11-09 11:55:08 -0400

    [RS_13] - List templates

--- src/app/views/template-forms/template-forms.component.ts
+++ src/app/views/template-forms/template-forms.component.ts
@@ -1,5 +1,6 @@
+import { TemplateItem } from './../../models/templateItem';
 import { Component, OnInit } from '@angular/core';
-import {TemplateItemComponent} from '../../components/template-item/template-item.component';
+import { TemplateItemComponent } from './template-item/template-item.component';
 
 @Component({
   selector: 'app-template-forms',
@@ -8,8 +9,22 @@ import {TemplateItemComponent} from '../../components/template-item/template-ite
 })
 export class TemplateFormsComponent implements OnInit {
 
+  templates: TemplateItem[] = new Array();
+
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    for (let i = 0 ; i < 30 ; i++) {
+      const t = new TemplateItem();
+      t.description = 'description test:' + i;
+      t.title = 'title test';
+      t.content = 'content test';
+      t.id = i;
+      this.templates.push(t);
+    }
+  }
 
+  createNewForm() {
+
+  }
 }
