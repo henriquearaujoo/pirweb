e6e3c7d0c64d93cc84a568480fce0c2a094dc61c
luiz ricardo <luiz.ricardo@institutoitn.com.br>
2017-12-05 09:59:58 -0400

    sprint2

---
+++ src/app/models/chapter.ts
@@ -0,0 +1,7 @@
+export class Chapter {
+    public id: number;
+    public title: string;
+    public description: string;
+    public type: any;
+    public content: any;
+}
---
+++ src/app/views/chapter/chapter.component.ts
@@ -0,0 +1,32 @@
+import { Chapter } from './../../models/chapter';
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ChapterItemComponent } from '../../components/chapter-item/chapter-item.component';
+
+@Component({
+  selector: 'app-chapter',
+  templateUrl: './chapter.component.html',
+  styleUrls: ['./chapter.component.css']
+})
+export class ChapterComponent implements OnInit {
+
+  mChapter: Chapter[] = new Array();
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    for (let i = 0 ; i < 10 ; i++) {
+      const t = new Chapter();
+      t.description = 'description test:' + i;
+      t.title = 'title test';
+      t.content = 'content test';
+      t.id = i;
+      this.mChapter.push(t);
+    }
+  }
+
+  createNewChapter() {
+    this.router.navigate(['information']);
+  }
+
+}
