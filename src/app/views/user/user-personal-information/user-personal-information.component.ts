be82fa23d5d2bd6d6b1243b2ee51c28c5256d29d
luiz ricardo <luiz.ricardo@institutoitn.com.br>
2017-12-11 10:37:50 -0400

    user information

---
+++ src/app/views/user/user-personal-information/user-personal-information.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-personal-information',
+  templateUrl: './user-personal-information.component.html',
+  styleUrls: ['./user-personal-information.component.css']
+})
+export class UserPersonalInformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
