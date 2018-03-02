7a7258def950e54b29f991427aea24a878942cce
Fabricia Galibi <fabricia.galibi@institutoitn.com.br>
2018-03-02 17:30:27 -0400

    fixed bugs

--- src/app/views/profile/page/page.component.ts
+++ src/app/views/profile/page/page.component.ts
@@ -26,8 +26,6 @@ import { AlertsService, AlertType } from '@jaspero/ng2-alerts';
 
 export class PageComponent extends PagenateComponent implements OnInit {
 
-  public selectedPages = new Array();
-  private pageFromServer = new Array();
   private all_pages: Page[] = new Array();
   private all_pages_profile: Rule[] = new Array();
   private pagesFromProfile = new Array();
@@ -36,19 +34,14 @@ export class PageComponent extends PagenateComponent implements OnInit {
   permissionsFromProfile: Rule[] = new Array();
   public page_allowed = new Array();
 
-  private selected: any[] = new Array();
-  private allowed_selected: any[] = new Array();
-  private matPermissions: boolean[][];
-  private confirm_rules: boolean;
-  private paginate: Paginate = new Paginate();
+  // private selected: any[] = new Array();
+  // private allowed_selected: any[] = new Array();
+  // private matPermissions: boolean[][];
+  // private confirm_rules: boolean;
+  // private paginate: Paginate = new Paginate();
   private profiles: Profile[] = new Array();
   private rule: Rule = new Rule();
-  private options: any[] = [
-    {'id': 1, 'rule': 'Criar'},
-    {'id': 2, 'rule': 'Editar'},
-    {'id': 3, 'rule': 'Visualizar'},
-    {'id': 4, 'rule': 'Desabilitar'}
-  ];
+
   private checked: any[] = new Array();
   private hasPermissions: boolean;
   @Input() insertValue: boolean;
@@ -70,12 +63,9 @@ export class PageComponent extends PagenateComponent implements OnInit {
     this.hasPermissions = false;
     this.currentProfile = this.accessPageService.getProfile();
     this.currentPage.id = '';
-    console.log('currentProfile', this.currentProfile.id);
     this.loadAllPages();
 
   }
-
-  ngOnChange() {}
 
   getCurrentProfile() {
     this.currentProfile = this.accessPageService.getProfile();
@@ -121,126 +111,29 @@ export class PageComponent extends PagenateComponent implements OnInit {
     );
   }
 
-  loadPageFromProfile_() {
-    this.profileService.getPages(this.accessPageService.getProfile().id).subscribe(
-      s => {
-        this.pagesFromProfile = s;
-        console.log('loadPageFromProfile_', this.pagesFromProfile);
-
-      },
-      e => console.log(e)
-    );
-  }
-
-  loadPageFromProfile() {
-    this.profileService.getPages(this.accessPageService.getProfile().id).subscribe(
-      s => {
-        this.pagesFromProfile = s;
-        console.log('loadPageFromProfile', this.pagesFromProfile);
-        this.accessPageService.getAllPages().subscribe(
-          s2 => {
-            this.pageFromServer = s2;
-            for ( let i = 0 ; i < this.pagesFromProfile.length; i++) {
-              for (let j = 0 ; j < this.pageFromServer.length; j++) {
-                if ( this.pagesFromProfile[i].route === this.pageFromServer[j].route) {
-                  this.pageFromServer.splice(j, 1);
-                }
-              }
-            }
-          },
-          e2 => {
-            console.log(e2);
-          }
-        );
-      },
-      e => console.log(e)
-    );
-  }
-
-  onReloadPage(event) {
-    this.loadPageFromProfile();
-    console.log(event);
-  }
-
-  updateSelected(option, event) {
-    console.log('event.target.value ' + event.target.value);
-    const index = this.selected.indexOf(option);
-    if (event.target.selected) {
-      console.log('insert');
-      if ( index === -1) {
-        this.selected.push(option);
-      }
-    } else {
-      console.log('delete');
-      if ( index !== -1) {
-        if ( ((index === 0) && (this.selected.length === 1))  ||
-        ((index >= 1) && (this.selected.length >= 1)) ) {
-          console.log('index delete []:', index);
-          console.log('selected.length:', this.selected.length);
-          this.selected = [];
-        }
-        this.selected.splice(index, 1);
-        console.log('index delete', index);
-      }
-    }
-    console.log('Selected', this.selected);
-  }
-
-  updateAllowedSelected(option, event) {
-    const index = this.allowed_selected.indexOf(option);
-    console.log('i index:', index);
-    if (event.target.selected) {
-      console.log('insert');
-      if ( index === -1) {
-        this.allowed_selected.push(option);
-      }
-    } else {
-      console.log('delete');
-      if ( index !== -1) {
-        if ( ((index === 0) && (this.allowed_selected.length === 1))  ||
-           ((index >= 1) && (this.allowed_selected.length >= 1)) ) {
-          console.log('index delete []:', index);
-          console.log('allowed_selected.length:', this.allowed_selected.length);
-          this.allowed_selected = [];
-        }
-        this.allowed_selected.splice(index, 1);
-        console.log('index delete', index);
-      }
-    }
-    console.log('Allowed Selected', this.allowed_selected);
-  }
-
-  confirmRules(confirm: boolean) {
-    this.confirm_rules = confirm;
-    if ( this.confirm_rules) {
-      this.selected = [];
-      this.allowed_selected = [];
-      this.confirm_rules = false;
-    }
-  }
-
-  setPages() {
-    if ( this.selectedPages.length > 0) {
-      this.accessPageService.setPages(this.selectedPages);
-    }
-  }
-
-  public removePages() {
-    for (let i = 0; i < this.page_allowed.length ; i++) {
-      this.ruleService.deleteRule(this.page_allowed[i].rules[0].id).subscribe(
-        s => {
-          this.loadPageFromProfile();
-          console.log('removed:' + this.page_allowed[i].rules[0].id);
-        },
-        e => {
-          this.loadPageFromProfile();
-          console.log('can`t remove the register' + this.page_allowed[i].rules[0].id);
-        }
-      );
-    }
-    this.allowed_selected = [];
-    this.selected = [];
-  }
+  // loadPageFromProfile() {
+  //   this.profileService.getPages(this.accessPageService.getProfile().id).subscribe(
+  //     s => {
+  //       this.pagesFromProfile = s;
+  //       this.accessPageService.getAllPages().subscribe(
+  //         s2 => {
+  //           this.pageFromServer = s2;
+  //           for ( let i = 0 ; i < this.pagesFromProfile.length; i++) {
+  //             for (let j = 0 ; j < this.pageFromServer.length; j++) {
+  //               if ( this.pagesFromProfile[i].route === this.pageFromServer[j].route) {
+  //                 this.pageFromServer.splice(j, 1);
+  //               }
+  //             }
+  //           }
+  //         },
+  //         e2 => {
+  //           console.log(e2);
+  //         }
+  //       );
+  //     },
+  //     e => console.log(e)
+  //   );
+  // }
 
   setProfile() {
     this.accessPageService.profileSelected(this.currentProfile);
@@ -256,7 +149,6 @@ export class PageComponent extends PagenateComponent implements OnInit {
         this.all_pages_profile = success_page_profile;
 
         // * CHECKED * /
-        console.log('event.target.value ' + event.target.value);
         const index = this.checked.indexOf(option);
 
         if (event.target.checked) {
@@ -296,9 +188,7 @@ export class PageComponent extends PagenateComponent implements OnInit {
                 break;
               }
             }
-          // }
         }  else {
-          // if ( index !== -1) {
             this.checked.splice(index, 1);
             switch (option) {
               case 1: {
