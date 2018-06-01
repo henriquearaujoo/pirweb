d46184bc190c2d7af8b7421095663b7c7e3dad4c
luiz ricardo <luiz.ricardo@institutoitn.com.br>
2018-06-01 19:01:54 -0400

    report dashboard and table

--- src/app/constant/constant.ts
+++ src/app/constant/constant.ts
@@ -2,10 +2,10 @@ export const Constant = {
     //  BASE_URL : 'http://10.10.3.88:8080/pir/rest/',
     BASE_URL : localStorage.getItem('server') ?
                localStorage.getItem('server') :
-            //    'http://10.10.3.85:8080/pir/rest/',
+               'http://10.10.3.85:8080/pir/rest/',
             //    'http://samsung.institutoitn.com.br/pir-dev/rest/',
             // 'http://10.10.3.148:8080/pir/rest/',
-            'http://localhost:8080/pir/rest/',
+            // 'http://localhost:8080/pir/rest/',
             // 'http://192.168.0.5:8080/pir/rest/',
     // BASE_URL : 'http://localhost:3000/',
     MEDIA_TYPE:
