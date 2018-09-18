da087eb1a0032ee31ea062fa9924a82be2fd88fb
Papa Neto <papaneto@hotmail.com>
2018-09-18 13:10:47 -0300

    inclusion of licences

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -7,98 +7,102 @@ import { HomeLayoutComponent } from './components/layout/home-layout.component';
 import { LoginLayoutComponent } from './components/layout/login-layout.component';
 
 const App_Router: Routes = [
-    {
-      path: '',
-      component: HomeLayoutComponent,
-      canActivate: [AuthGuard],
-      children: [
-        {
-          path: '',
-          redirectTo: 'home',
-          pathMatch: 'full',
-        },
-        {
-          path: 'home', component: HomeComponent
-        },
-        {
-          path: 'capitulos',
-          loadChildren: './views/template-chapter/template-chapter.module#TemplateChapterModule'
-        },
-        {
-          path: 'usuarios',
-          loadChildren: './views/user/user.module#UserModule'
-        },
-        {
-          path: 'agente-dashboard',
-          loadChildren: './views/agent/agent.module#AgentModule'
-        },
-        {
-          path: 'agente-visita',
-          loadChildren: './views/agent/visit-history/visit-history.module#VisitHistoryModule'
-        },
-        {
-          path: 'agente-desempenho',
-          loadChildren: './views/agent/performance-chart/performance-chart.module#PerformanceChartModule'
-        },
-        {
-          path: 'agentes-mapa',
-          loadChildren: './views/agent/agent-map/agent-map.module#AgentMapModule'
-        },
-        {
-          path: 'perfis',
-          loadChildren: './views/profile/profile.module#ProfileModule'
-        },
-        {
-          path: 'familias',
-          loadChildren: './views/responsible/responsible.module#ResponsibleModule'
-        },
-        {
-          path: 'gestantes',
-          loadChildren: './views/pregnant/pregnant.module#PregnantModule'
-        },
-        {
-          path: 'criancas',
-          loadChildren: './views/child/child.module#ChildModule'
-        },
-        {
-          path: 'comunidades',
-          loadChildren: './views/community/community.module#CommunityModule'
-        },
-        {
-          path: 'regionais',
-          loadChildren: './views/regional/regional.module#RegionalModule'
-        },
-        // {
-        //   path: 'unidades',
-        //   loadChildren: './views/uc/uc.module#UcModule'
-        // },
-        {
-          path: 'formularios',
-          loadChildren: './views/form-template/form-template.module#FormTemplateModule'
-        },
-        {
-          path: 'dashboard',
-          loadChildren: './views/dashboard/dashboard.module#DashboardModule'
-        },
-        {
-          path: 'relatorios',
-          loadChildren: './views/report/report.module#ReportModule'
-        },
-      ]
-    },
-    {
-      path: '',
-      component: LoginLayoutComponent,
-      children: [
-        {
-          path: 'login',
-          loadChildren: './views/login/login.module#LoginModule'
-        },
-      ]
-    },
-    // { path: 'send-email', component: SendEmailComponent},
-    // { path: 'reset-password', component: ResetPasswordComponent},
-    { path: '**', redirectTo: '' }
+  {
+    path: '',
+    component: HomeLayoutComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home', component: HomeComponent
+      },
+      {
+        path: 'capitulos',
+        loadChildren: './views/template-chapter/template-chapter.module#TemplateChapterModule'
+      },
+      {
+        path: 'usuarios',
+        loadChildren: './views/user/user.module#UserModule'
+      },
+      {
+        path: 'agente-dashboard',
+        loadChildren: './views/agent/agent.module#AgentModule'
+      },
+      {
+        path: 'agente-visita',
+        loadChildren: './views/agent/visit-history/visit-history.module#VisitHistoryModule'
+      },
+      {
+        path: 'agente-desempenho',
+        loadChildren: './views/agent/performance-chart/performance-chart.module#PerformanceChartModule'
+      },
+      {
+        path: 'agentes-mapa',
+        loadChildren: './views/agent/agent-map/agent-map.module#AgentMapModule'
+      },
+      {
+        path: 'perfis',
+        loadChildren: './views/profile/profile.module#ProfileModule'
+      },
+      {
+        path: 'familias',
+        loadChildren: './views/responsible/responsible.module#ResponsibleModule'
+      },
+      {
+        path: 'gestantes',
+        loadChildren: './views/pregnant/pregnant.module#PregnantModule'
+      },
+      {
+        path: 'criancas',
+        loadChildren: './views/child/child.module#ChildModule'
+      },
+      {
+        path: 'comunidades',
+        loadChildren: './views/community/community.module#CommunityModule'
+      },
+      {
+        path: 'regionais',
+        loadChildren: './views/regional/regional.module#RegionalModule'
+      },
+      // {
+      //   path: 'unidades',
+      //   loadChildren: './views/uc/uc.module#UcModule'
+      // },
+      {
+        path: 'formularios',
+        loadChildren: './views/form-template/form-template.module#FormTemplateModule'
+      },
+      {
+        path: 'dashboard',
+        loadChildren: './views/dashboard/dashboard.module#DashboardModule'
+      },
+      {
+        path: 'relatorios',
+        loadChildren: './views/report/report.module#ReportModule'
+      },
+      {
+        path: 'licences',
+        loadChildren: './views/about/about.module#AboutModule'
+      }
+    ]
+  },
+  {
+    path: '',
+    component: LoginLayoutComponent,
+    children: [
+      {
+        path: 'login',
+        loadChildren: './views/login/login.module#LoginModule'
+      },
+    ]
+  },
+  // { path: 'send-email', component: SendEmailComponent},
+  // { path: 'reset-password', component: ResetPasswordComponent},
+  { path: '**', redirectTo: '' }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(App_Router);
